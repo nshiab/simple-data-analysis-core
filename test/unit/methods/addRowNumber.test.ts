e62835d7778753a33bccc7fe62cdea74156fbe88
Nael Shiab <nael.shiab@gmail.com>
2026-05-26 12:53:25 -0400

    feat: enhance addRowNumber method to support category-based row numbering (#61)
    Co-authored-by: nshiab <nshiab@users.noreply.github.com>

--- test/unit/methods/addRowNumber.test.ts
+++ test/unit/methods/addRowNumber.test.ts
@@ -13,8 +13,30 @@ Deno.test("should return a column with the row number", async () => {
   const data = await table.getData();
 
   assertEquals(data, [
-    { first: "Nael", last: "Shiab", rowNumber: 1 },
-    { first: "Graeme", last: "Bruce", rowNumber: 2 },
+    { first: "Nael", last: "Shiab", rowNumber: 0 },
+    { first: "Graeme", last: "Bruce", rowNumber: 1 },
+  ]);
+  await sdb.done();
+});
+
+Deno.test("should return a column with the row number restarting for each category (starting at 0)", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("data");
+  await table.loadArray([
+    { first: "Nael", last: "Shiab", city: "Montreal" },
+    { first: "Graeme", last: "Bruce", city: "Montreal" },
+    { first: "John", last: "Doe", city: "Toronto" },
+    { first: "Jane", last: "Doe", city: "Toronto" },
+  ]);
+  await table.addRowNumber("rowNumber", { categories: "city" });
+
+  const data = await table.getData();
+
+  assertEquals(data, [
+    { first: "Nael", last: "Shiab", city: "Montreal", rowNumber: 0 },
+    { first: "Graeme", last: "Bruce", city: "Montreal", rowNumber: 1 },
+    { first: "John", last: "Doe", city: "Toronto", rowNumber: 0 },
+    { first: "Jane", last: "Doe", city: "Toronto", rowNumber: 1 },
   ]);
   await sdb.done();
 });
