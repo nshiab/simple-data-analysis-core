c1724515f1736e4c1c0990d98837c97e83be25a3
Nael Shiab <nael.shiab@gmail.com>
2026-05-26 12:54:05 -0400

    Better performance for randomPoint (#62)
    * feat: enhance getNbRows and randomPoint methods with options for conditions and error handling
    
    * feat: improve randomPoint performance with early exit and add test for scalability
    
    ---------
    
    Co-authored-by: nshiab <nshiab@users.noreply.github.com>

--- test/unit/methods/randomPoint.test.ts
+++ test/unit/methods/randomPoint.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals, assertNotEquals } from "@std/assert";
+import { assertEquals, assertNotEquals, assertRejects } from "@std/assert";
 import SimpleDB from "../../../src/class/SimpleDB.ts";
 
 Deno.test("should generate a random point in geometries and they should be random", async () => {
@@ -52,6 +52,95 @@ Deno.test("should generate a random point in geometries and they should be rando
       }`,
     );
   }
+
+  await sdb.done();
+});
+
+Deno.test("randomPoint should throw an error if no point is found", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("geodata");
+  await table.loadGeoData(
+    "test/geodata/files/CanadianProvincesAndTerritories.json",
+  );
+
+  // With 0 tries, no point should be found.
+  await assertRejects(
+    async () => {
+      await table.randomPoint("randomPoint", 0);
+    },
+    Error,
+    "13 points could not be generated. Consider increasing nbPointsToTry or set options.try to true.",
+  );
+
+  await sdb.done();
+});
+
+Deno.test("randomPoint should not throw an error if no point is found and options.try is true", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("geodata");
+  await table.loadGeoData(
+    "test/geodata/files/CanadianProvincesAndTerritories.json",
+  );
+
+  // With 0 tries, no point should be found.
+  await table.randomPoint("randomPoint", 0, { try: true });
+
+  const nbNulls = await table.getNbRows({
+    conditions: '"randomPoint" IS NULL',
+  });
+  assertEquals(nbNulls, 13);
+
+  await sdb.done();
+});
+
+Deno.test("randomPoint should throw an error if nbPointsToTry is less than 0", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("geodata");
+  await table.loadGeoData(
+    "test/geodata/files/CanadianProvincesAndTerritories.json",
+  );
+
+  await assertRejects(
+    async () => {
+      // @ts-ignore trying invalid value
+      await table.randomPoint("randomPoint", -1);
+    },
+    Error,
+    "nbPointsToTry must be a number greater than or equal to 0",
+  );
+
+  await sdb.done();
+});
+
+Deno.test("randomPoint should have similar performance with many more tries if it exits early", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("geodata");
+  await table.loadGeoData(
+    "test/geodata/files/CanadianProvincesAndTerritories.json",
+  );
+
+  const start10 = Date.now();
+  await table.randomPoint("point10", 10, { column: "geom", try: true });
+  const duration10 = Date.now() - start10;
+
+  const start10k = Date.now();
+  await table.randomPoint("point10k", 10_000, { column: "geom", try: true });
+  const duration10k = Date.now() - start10k;
+
+  const start100k = Date.now();
+  await table.randomPoint("point100k", 100_000, { column: "geom", try: true });
+  const duration100k = Date.now() - start100k;
+
+  console.log({ duration10, duration10k, duration100k });
+
+  // For 13 provinces, finding one point is fast.
+  // DuckDB doesn't always short-circuit range() in LATERAL joins with a WHERE clause,
+  // so the duration might scale more linearly than expected with tries.
+  assertEquals(
+    duration100k < 10000,
+    true,
+    `Performance issue: 100k tries took ${duration100k}ms.`,
+  );
 
   await sdb.done();
 });
