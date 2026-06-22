b8cfb3d9b773f03cf42e631de0725457d48d9ac0
Nael Shiab <nael.shiab@gmail.com>
2026-06-22 10:11:07 -0400

    Add "all" option to replace() and replaceNulls() to target every column (#74)
    Closes #71
    
    By: nshiab <nshiab@users.noreply.github.com>

--- test/unit/methods/replaceNulls.test.ts
+++ test/unit/methods/replaceNulls.test.ts
@@ -46,3 +46,26 @@ Deno.test("should replace null values in multiple columns", async () => {
 
   await sdb.done();
 });
+
+Deno.test("should replace null values in all columns with the 'all' option", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable();
+  await table.loadArray([
+    { keyA: 1, keyB: 1, keyC: 1 },
+    { keyA: null, keyB: 2, keyC: null },
+    { keyA: 3, keyB: null, keyC: 3 },
+    { keyA: null, keyB: 4, keyC: null },
+  ]);
+  await table.replaceNulls("all", 0);
+
+  const data = await table.getData();
+
+  assertEquals(data, [
+    { keyA: 1, keyB: 1, keyC: 1 },
+    { keyA: 0, keyB: 2, keyC: 0 },
+    { keyA: 3, keyB: 0, keyC: 3 },
+    { keyA: 0, keyB: 4, keyC: 0 },
+  ]);
+
+  await sdb.done();
+});
